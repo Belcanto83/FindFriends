e771a8d47d3b3c22895d6fece1bcab33559171e3
Alex <farego@yandex.ru>
2022-11-10 18:34:08 +0300

    Basic peer user search done

--- vk_bot/VK_bot.py
+++ vk_bot/VK_bot.py
@@ -2,6 +2,7 @@ import requests
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 import json
+from datetime import datetime
 
 from vkinder_database.models import User, UserMark, Mark
 from vkinder_database.postgres_db import VKinderPostgresqlDB
@@ -21,6 +22,20 @@ class KeyBoardMaker:
         keyboard.add_button('Начать', VkKeyboardColor.PRIMARY)
         return keyboard
 
+    @staticmethod
+    def _keyboard_find_next_peer():
+        keyboard = VkKeyboard(one_time=True)
+        keyboard.add_button('Следующий', VkKeyboardColor.SECONDARY)
+        keyboard.add_line()
+        keyboard.add_button('В Избранное', VkKeyboardColor.POSITIVE)
+        keyboard.add_button('В Черный список', VkKeyboardColor.NEGATIVE)
+        keyboard.add_line()
+        keyboard.add_button('Избранное', VkKeyboardColor.POSITIVE)
+        keyboard.add_button('Черный список', VkKeyboardColor.NEGATIVE)
+        keyboard.add_line()
+        keyboard.add_button('Начать', VkKeyboardColor.PRIMARY)
+        return keyboard
+
 
 class VkBot(KeyBoardMaker):
     base_url = 'https://api.vk.com/method/'
@@ -30,16 +45,21 @@ class VkBot(KeyBoardMaker):
         self.vk_api_version = vk_api_version
         self.params = dict(access_token=bot_token, v=vk_api_version)
         self.user_id = user_id
+        self.peer_user_info = {}
+
         self.bot_menu = {
             'начать': {'func': self._message_start, 'args': (), 'keyboard': self._keyboard_start},
-            # 'мужчина': {'func': self._get_peer_user_info, 'args': (2,), 'keyboard': self._keyboard_start},
-            # 'женщина': {'func': self._get_peer_user_info, 'args': (1,), 'keyboard': self._keyboard_start},
+            'мужчина': {'func': self._message_get_peer_user_info, 'args': (2,),
+                        'keyboard': self._keyboard_find_next_peer},
+            'женщина': {'func': self._message_get_peer_user_info, 'args': (1,),
+                        'keyboard': self._keyboard_find_next_peer},
+            'следующий': {'func': self._message_get_next_peer, 'args': (),
+                          'keyboard': self._keyboard_find_next_peer},
         }
 
         # TODO 1: записать информацию о VK_ID пользователя бота в БД
         self._add_user_to_db()
         self.user_info = self._get_user_info_from_vk_id(user_id)
-        self.peer_user_info = {}
 
     def _get_user_info_from_vk_id(self, user_id):
         method_url = self.base_url + 'users.get'
@@ -61,9 +81,22 @@ class VkBot(KeyBoardMaker):
 
         return message
 
-    def _get_peer_user_info(self, sex_id):
+    def _message_get_peer_user_info(self, sex_id):
         self.peer_user_info['sex'] = sex_id
         self.peer_user_generator = self._find_peer()
+        # print('Generator', list(self.peer_user_generator))
+        peer_user = next(self.peer_user_generator)
+        message = f"{peer_user.get('first_name')} {peer_user.get('last_name')}\n" \
+                  f"https://vk.com/id{peer_user.get('id')}\n"
+
+        return message
+
+    def _message_get_next_peer(self):
+        peer_user = next(self.peer_user_generator)
+        message = f"{peer_user.get('first_name')} {peer_user.get('last_name')}\n" \
+                  f"https://vk.com/id{peer_user.get('id')}\n"
+
+        return message
 
     def _find_peer(self):
         def get_peer_users_generator():
@@ -83,14 +116,13 @@ class VkBot(KeyBoardMaker):
             'v': self.vk_api_version,
             'city_id': self.user_info.get('city').get('id'),
             'sex': self.peer_user_info.get('sex'),
-            'age_from': int(self.user_info.get('bdate')[-4:]) - 5,
-            'age_to': int(self.user_info.get('bdate')[-4:]) + 5,
+            'age_from': (datetime.now().year - int(self.user_info.get('bdate')[-4:])) - 5,
+            'age_to': (datetime.now().year - int(self.user_info.get('bdate')[-4:])) + 5,
         }
-        # response = requests.get(method_url, params=all_params).json()
-        return get_peer_users_generator()
+        # resp = requests.get(method_url, params=method_params).json()
+        # print('Response:', resp)
 
-    def _next_friend(self):
-        return next(self.peer_user_generator)
+        return get_peer_users_generator()
 
     def _add_user_to_db(self):
         db_creds_path = 'info_not_for_git/postgresql.json'
@@ -109,20 +141,13 @@ class VkBot(KeyBoardMaker):
         except OperationalError as err:
             print('Ошибка подключения к БД:', err)
 
-    # @staticmethod
-    # def _keyboard_start():
-    #     keyboard = VkKeyboard(one_time=True)
-    #     keyboard.add_button('Мужчина', VkKeyboardColor.SECONDARY)
-    #     keyboard.add_button('Женщина', VkKeyboardColor.SECONDARY)
-    #     return keyboard
-
     def new_message(self, request):
         if request in self.bot_menu:
             action = self.bot_menu.get(request)
             message = action.get('func')(*action.get('args'))
             return message
         else:
-            message = "Я пока такое не умею.."
+            message = "Я пока такое не умею..\nЧтобы начать все заново, напиши 'начать'.."
             return message
 
     def new_keyboard(self, request):
